e6b1d1fdac69a910e96d214ddfbbd2e2fc564f6e
Turgay Mammadov <memmedovturqay871@gmail.com>
2023-05-13 20:18:35 +0400

    updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -11,7 +11,42 @@ const [open, setOpen] = useState(false);
 <h4>This is what demonstrates my knowledge</h4> 
 <span className="bottom-line"></span>
 </div>
-<Tab.Container id="left-tabs-example" defaultActiveKey="first" className="mt-4">
+<div className='portfolio-container container'>
+<article className='portfolio-item'>
+<div className='portfolio-img'>
+<img src='ecommerce.png'/>
+</div>
+<h4>E-commerce app</h4>
+<div className='portfolio-links'>
+<a href='https://github.com/turqay667/e-commerce'  className='btn btn-success' target='_'>GitHub</a>
+<a href="" target='_' className='btn btn-primary'>Live Demo</a>
+</div>
+
+</article>
+<article className='portfolio-item'>
+<div className='portfolio-img'>
+<img src='forecast.png'/>
+</div>
+<h4>Weather App</h4>
+<div className='portfolio-links'>
+<a href='https://github.com/' className='btn btn-success'  target='_'>GitHub</a>
+<a href="https://lastforecast.netlify.app/" className='btn btn-primary' target='_'>Live Demo</a>
+</div>
+</article>
+<article className='portfolio-item'>
+<div className='portfolio-img'>
+<img src='advice_generator.png'/>
+</div>
+<h4> Advice Generator App</h4>
+<div className='portfolio-links'>
+<a href='https://github.com/turqay667/advice_generator' target='_' className='btn btn-success'>GitHub</a>
+<a href="https://turqay667.github.io/advice_generator/" className='btn btn-primary' target='_'>Live Demo</a>
+</div>
+</article>
+</div>
+
+
+{/* <Tab.Container id="left-tabs-example" defaultActiveKey="first" className="mt-4">
 <Nav variant="pills" className="flex-row justify-content-center mb-5 mt-5">
 <Nav.Item>
               <Nav.Link eventKey="second">Web Design</Nav.Link>
@@ -36,7 +71,7 @@ const [open, setOpen] = useState(false);
             <div>  <img src="dashboard.png"/>  </div>
             </Tab.Pane>
           </Tab.Content>
-          </Tab.Container>
+          </Tab.Container> */}
          
 </section>
     )
