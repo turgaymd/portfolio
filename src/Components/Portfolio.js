f1bd04945246d6686f5a45aa75029e18e1fa1bf2
turgaymammadov <memmedovturqay871@gmail.com>
2023-08-25 00:22:09 +0400

    updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -19,7 +19,7 @@ const [open, setOpen] = useState(false);
 <h4>E-commerce app</h4>
 <div className='portfolio-links'>
 <a href='https://github.com/turqay667/e-commerce'  className='btn btn-success' target='_'>GitHub</a>
-<a href="" target='_' className='btn btn-primary'>Live Demo</a>
+<a href="https://acessories-d3d34065ee2d.herokuapp.com" target='_' className='btn btn-primary'>Live Demo</a>
 </div>
 
 </article>
@@ -46,33 +46,7 @@ const [open, setOpen] = useState(false);
 </div>
 
 
-{/* <Tab.Container id="left-tabs-example" defaultActiveKey="first" className="mt-4">
-<Nav variant="pills" className="flex-row justify-content-center mb-5 mt-5">
-<Nav.Item>
-              <Nav.Link eventKey="second">Web Design</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link eventKey="first">Admin Dashboard</Nav.Link>
-            </Nav.Item>
-          
-            <Nav.Item>
-              <Nav.Link eventKey="third">E-commerce</Nav.Link>
-            </Nav.Item>
-          </Nav> 
-         <Tab.Content className='df'>
-            <Tab.Pane eventKey="third">
-            <div><img src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
-            </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="second">
-            <div><img src="web_service.png"/> </div>
-            </Tab.Pane>
-            <Tab.Pane eventKey="first">
-            <div>  <img src="dashboard.png"/>  </div>
-            </Tab.Pane>
-          </Tab.Content>
-          </Tab.Container> */}
-         
+
 </section>
     )
 }
