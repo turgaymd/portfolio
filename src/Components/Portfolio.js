b16bc6ef6d7af9f2626c2d8f021b8535c2feddf9
turgaymammadov <memmedovturqay871@gmail.com>
2025-01-22 14:28:42 +0400

    updated

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -15,10 +15,10 @@ const [open, setOpen] = useState(false);
 <div className='portfolio-img'>
 <img src='ecommerce.jpeg'/>
 </div>
-<a className='pt-3'>Modern E-commerce </a>
 <div className='portfolio-links'>
+<h6>Modern E-commerce </h6>
 {/* <a href='https://github.com/turqay667/accessories.az' className='btn btn-success' target='_blank'>GitHub</a> */}
-<a href="https://accessories-az.onrender.com/" target='_blank' className='btn btn-primary'>Live Demo</a>
+<a href="https://accessories-az.onrender.com/" target='_blank' className='btn'>Live Demo</a>
 </div>
 
 </article>
@@ -26,40 +26,44 @@ const [open, setOpen] = useState(false);
 <div className='portfolio-img'>
 <img src='forecast.jpeg'/>
 </div>
-<a className='pt-3'>Weather Forecast</a>
+
 <div className='portfolio-links'>
+<h6>Weather Forecast</h6>
 {/* <a href='https://github.com/turqay667/weather-app' className='btn btn-success'  target='_blank'>GitHub</a> */}
-<a href="https://lastforecast.netlify.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+<a href="https://lastforecast.netlify.app/" className='btn' target='_blank'>Live Demo</a>
 </div>
 </article>
 <article className='portfolio-item'>
 <div className='portfolio-img'>
 <img src='chat_app.jpeg'/>
 </div>
-<a className='pt-3'>Real-Time Chat </a>
+
 <div className='portfolio-links'>
+<h6>Real-Time Chat </h6>
 {/* <a href='https://github.com/turqay667/chat_app' className='btn btn-success'  target='_blank'>GitHub</a> */}
-<a href="https://chat-app-bxnf.vercel.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+<a href="https://chat-app-bxnf.vercel.app/" className='btn' target='_blank'>Live Demo</a>
 </div>
 </article>
 <article className='portfolio-item'>
 <div className='portfolio-img'>
 <img src='tictac.jpeg'/>
 </div>
-<a className='pt-3'>  TIC TAC TOE </a>
+
 <div className='portfolio-links'>
+<h6>  TIC TAC TOE </h6>
 {/* <a href='https://github.com/turqay667/tic-tac-toe' target='_blank' className='btn btn-success'>GitHub</a> */}
-<a href="https://tic-tac-toe-jm6h.onrender.com/" className='btn btn-primary' target='_blank'>Live Demo</a>
+<a href="https://tic-tac-toe-jm6h.onrender.com/" className='btn' target='_blank'>Live Demo</a>
 </div>
 </article>
 <article className='portfolio-item'>
 <div className='portfolio-img'>
 <img src='note_app.jpeg'/>
 </div>
-<a className='pt-3'>  Note Taking App </a>
+
 <div className='portfolio-links'>
+<h6>  Note Taking App </h6>
 {/* <a href='https://github.com/turqay667/note-taking' target='_blank' className='btn btn-success'>GitHub</a> */}
-<a href="https://note-taking-livid.vercel.app/" className='btn btn-primary' target='_blank'>Live Demo</a>
+<a href="https://note-taking-livid.vercel.app/" className='btn' target='_blank'>Live Demo</a>
 
 </div>
 </article>
@@ -67,11 +71,11 @@ const [open, setOpen] = useState(false);
 <div className='portfolio-img'>
 <img src='advice.jpeg'/>
 </div>
-<a className='pt-3'>  Advice Generator </a>
-<div className='portfolio-links'>
-{/* <a href='https://github.com/turqay667/advice_generator' target='_blank' className='btn btn-success'>GitHub</a> */}
-<a href="https://turqay667.github.io/advice_generator/" className='btn btn-primary' target='_blank'>Live Demo</a>
 
+<div className='portfolio-links'>
+<h6>  Advice Generator </h6>
+{/* <a href='https://github.com/turqay667/advice_generator' target='_blank' className='btn btn-success'>GitHub</a> */}
+<a href="https://turqay667.github.io/advice_generator/" className='btn ' target='_blank'>Live Demo</a>
 </div>
 </article>
 </div>
