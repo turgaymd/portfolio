3d7b155792bc67b9c82a1040b3e85c48f5c368df
turgaymammadov <memmedovturqay871@gmail.com>
2025-01-30 13:47:42 +0400

    a

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,10 +3,11 @@
 const Home=()=>{
 return (
     <div className="admin">
+            <h1>Hello, I'm  </h1>
             <div className="typewriter">
-    <h1>Hello, I'm <span className="text-info">Turgay Mammadov</span> </h1>
+            <h2 className="text-info">Turgay Mammadov</h2>
         </div>
-    <h4 className=" pb-2">Front-end Developer</h4>
+    {/* <h4 className=" pb-2">Front-end Developer</h4> */}
          <div className="pt-4 pb-4">
          <a className="submit-btn" href="#contact">Hire me</a> 
             </div>
