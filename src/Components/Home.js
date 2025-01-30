a27b2ec30ac5f3bbcb6cb2a8fb6a58b32332bc04
turgaymammadov <memmedovturqay871@gmail.com>
2025-01-30 14:11:17 +0400

    a

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ return (
     <div className="admin">
             <h1>Hello, I'm  </h1>
             <div className="typewriter">
-            <h2 ><span className="text-info">Turgay Mammadov,</span> front-end developer <br></br> based in Azerbaijan</h2> 
+            <h2 ><span className="text-info">Turgay Mammadov,</span> front-end developer</h2> 
         </div>
     {/* <h4 className=" pb-2">Front-end Developer</h4> */}
          <div className="pt-4 pb-4">
