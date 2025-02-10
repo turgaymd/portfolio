70a5f68ba8801d1385b692781ac49bab289e75e1
turgaymammadov <memmedovturqay871@gmail.com>
2025-02-10 13:24:40 +0400

    ff

--- src/Components/Skill.js
+++ src/Components/Skill.js
@@ -1,67 +1,61 @@
-import { FaReact } from "react-icons/fa";
-import { FaJs } from "react-icons/fa";
-import { FaFigma } from "react-icons/fa";
+
+
 const Skills=()=>{
+  const techs=[
+    {
+        title:'Vanilla JS',
+        img:'js.png'
+    },
+    {
+      title:'React',
+      img:'React.png'
+    },
+    {
+      title:'Node.js',
+      img:'Node.js.png'
+    },
+    {
+      title:'MongoDB',
+      img:'MongoDB.png'
+    },
+    {
+      title:'Next.js',
+      img:'Next.js.png'
+    },
+    {
+      title:'Tailwind CSS',
+      img:'tailwind.png'
+    },
+    {
+      title:'Git',
+      img:'Git.png'
+    },
+    {
+      title:'Figma',
+      img:'Figma.png'
+    },
+    ]
     return(
         <div className="container"> 
             <section id="skills" className="text-center">
             <h2>Skills</h2>
             <h4 className="text-center mb-4"> My expertise</h4>
             <div className="row pb-4 pt-4">
-            <div className="skill col-md-3 col-6">
-            <a>
-            <img src="js.png"/>
-            </a>
-            <h6>Vanilla JS</h6>
+              {
+                techs.map((tech)=>{
+return(
+  <div className="skill col-md-3 col-6">
+  <div>
+  <img src={tech.img} alt="skill"/>
+  </div>
+  <h6>{tech.title}</h6>
+</div>
+)
+                })
+              }
+          
+  
           </div>
-         
-          <div className="skill col-md-3 col-6">
-            <a>
-            <img src="React.png"/>
-            </a>
-            <h6>React</h6>
-          </div>
-          <div className="skill col-md-3 col-6">
-            <a>
-            <img src="Node.js.png"/>
-            </a>
-            <h6>Node.js</h6>
-          </div> 
-          <div className="skill col-md-3 col-6">
-            <a>
-            <img src="MongoDB.png"/>
-            </a>
-            <h6>MongoDB</h6>
-          </div>
-          </div>
-          <div className="row pt-4 pb-4">
-          <div className="skill col-md-3 col-6">
-            <a>
-            <img src="next.js.png"/>
-            </a>
-            <h6>Next.js</h6>
-          </div>
-          <div className="skill col-md-3 col-6">
-            <a>
-           <img src="tailwind.png"/>
-            </a>
-            <h6>Tailwind CSS</h6>
-          </div>
-       
-          <div className="skill col-md-3 col-6">
-            <a>
-            <img src="Git.png"/>
-            </a>
-            <h6>Git</h6>
-          </div>
-          <div className="skill col-md-3 col-6">
-            <a>
-            <img src="Figma.png"/>
-            </a>
-            <h6>Figma</h6>
-          </div>
-         
-            </div>
             </section>
         </div>
     )
