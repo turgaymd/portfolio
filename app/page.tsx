1758a95311058c48cc1c6328950d287c342a5560
turgaymammadov <memedovturqay871@gmail.com>
2025-10-30 00:42:10 +0400

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,21 @@
-
 import About from "./Components/About";
-import Contact from "./Components/Contact"
-import Footer from "./Components/Footer"
-import Header from "./Components/Header"
-import Skills from './Components/Skill'
-import Services from "./Components/Services"
-import Portfolio from "./Components/Portfolio"
-export default function Home() {
+import React from "react";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Header from "./Components/Header";
+import Skills from "./Components/Skill";
+import Services from "./Components/Services";
+import Portfolio from "./Components/Portfolio";
+export default function Home():React.JSX.Element {
   return (
     <main>
-       <Header/>
-       <About/>
-       <Services/>
-       <Portfolio/>
-       <Skills/>
-       <Contact/>
-       <Footer/>
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Skills />
+      <Contact />
+      <Footer />
     </main>
   );
 }
