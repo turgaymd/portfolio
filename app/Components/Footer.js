cef312bbe282964d6f1a037f6b9dceeb788347a8
turgaymammadov <memedovturqay871@gmail.com>
2025-11-07 20:31:24 +0400

    update

--- app/Components/Footer.js
+++ app/Components/Footer.js
@@ -1,45 +1,71 @@
-"use client"
-import {ImFacebook} from "react-icons/im"
-import {FaAngleUp,FaInstagram, FaLinkedinIn} from "react-icons/fa"
+"use client";
+import { ImFacebook } from "react-icons/im";
+import { FaAngleUp, FaInstagram, FaLinkedinIn } from "react-icons/fa";
 import { useState } from "react";
 import { useEffect } from "react";
-const Footer=()=>{
-const [visible, setVisible]=useState(false)
-const currentDate=new Date().getFullYear()
-const handleScroll=()=>{
-    if(window.scrollY>400){
-        setVisible(true)
-    }  
-    else{
-        setVisible(false)
+const Footer = () => {
+  const [visible, setVisible] = useState(false);
+  const currentDate = new Date().getFullYear();
+  const handleScroll = () => {
+    if (window.scrollY > 400) {
+      setVisible(true);
+    } else {
+      setVisible(false);
     }
-}
-const scrollTop=()=>{
+  };
+  const scrollTop = () => {
     window.scrollTo({
-        top:0,
-    })
-}
-useEffect(()=>{
-    window.addEventListener("scroll",handleScroll)
-})
-    return(
-<footer>
-    <a href="#" className="footer_logo">Turgay</a>
-<div>
-    <div className="social">
- <a className="text-white" href="https://www.facebook.com/turqay.memmedov.9480" target="_blank" rel="noreferrer"><ImFacebook/></a>
-<a className="text-white" href="https://www.instagram.com/turqay.mdv/" target="_blank" rel="noreferrer"><FaInstagram/></a>
-<a className="text-white" href="https://www.linkedin.com/in/turgay-mammadov/" target="_blank" rel="noreferrer"><FaLinkedinIn/></a>
-    </div>
-<p className="footer_copyright mt-4">   © {currentDate} Portfolio - All Rights Reserved</p>
-</div>
-<div >
-{
-    visible ? <FaAngleUp  fontSize={40} className="topbutton" onClick={scrollTop}/> : <div></div>
-}
-
-</div>
-</footer>
-    )
-}
+      top: 0,
+    });
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  });
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        Turgay
+      </a>
+      <div>
+        <div className="social">
+          <a
+            className="text-white"
+            href="https://www.facebook.com/turqay.memmedov.9480"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <ImFacebook />
+          </a>
+          <a
+            className="text-white"
+            href="https://www.instagram.com/turqay.mdv/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            className="text-white"
+            href="https://www.linkedin.com/in/turgay-mammadov/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FaLinkedinIn />
+          </a>
+        </div>
+        <p className="footer_copyright mt-4">
+          {" "}
+          © {currentDate} Portfolio - All Rights Reserved
+        </p>
+      </div>
+      <div>
+        {visible ? (
+          <FaAngleUp fontSize={40} className="topbutton" onClick={scrollTop} />
+        ) : (
+          <div></div>
+        )}
+      </div>
+    </footer>
+  );
+};
 export default Footer;
--- app/Components/Services.js
+++ app/Components/Services.js
@@ -1,50 +1,52 @@
-"use client"
-const Services=()=>{
-    return(
-        <div>
-            <section id="services">
-<h2>Services</h2>
-<h4 className="text-center">What I am Doing</h4>
-<div className="container services_container">
- <article className="service">
- <div className="service_head"> 
-<a className="text-info"><ion-icon name="code-outline"></ion-icon></a>
-<h5>Web Development</h5>
-</div>
-<ul><li>Building websites that are responsive</li>
-<li>Integrating data from various back-end services and databases</li>
-
-</ul>
-</article>
-<article className="service"> 
-<div className="service_head"> 
-<a ><ion-icon name="file-tray-full-outline"></ion-icon></a>
-<h5>Web Scraping</h5>
-</div>
-<ul>
-    <li>Develop scripts to extract data
-    </li>
-    <li>Implement web scraping solutions       
-    </li>
-</ul>
-</article>
-<article className="service">
-<div className="service_head"> 
-<a ><ion-icon name="trending-up"></ion-icon></a>
-<h5>Search Engine Optimization</h5>
-</div>
-<ul>
-    
- 
-        <li>Execute SEO strategies
-            </li>
-            <li>Performing bug fixes
-            </li>
+"use client";
+const Services = () => {
+  return (
+    <div>
+      <section id="services">
+        <h2>Services</h2>
+        <h4 className="text-center">What I am Doing</h4>
+        <div className="container services_container">
+          <article className="service">
+            <div className="service_head">
+              <a className="text-info">
+                <ion-icon name="code-outline"></ion-icon>
+              </a>
+              <h5>Web Development</h5>
+            </div>
+            <ul>
+              <li>Building websites that are responsive</li>
+              <li>
+                Integrating data from various back-end services and databases
+              </li>
             </ul>
-</article>
-</div>
-            </section>
+          </article>
+          <article className="service">
+            <div className="service_head">
+              <a>
+                <ion-icon name="file-tray-full-outline"></ion-icon>
+              </a>
+              <h5>Web Scraping</h5>
+            </div>
+            <ul>
+              <li>Develop scripts to extract data</li>
+              <li>Implement web scraping solutions</li>
+            </ul>
+          </article>
+          <article className="service">
+            <div className="service_head">
+              <a>
+                <ion-icon name="trending-up"></ion-icon>
+              </a>
+              <h5>Search Engine Optimization</h5>
+            </div>
+            <ul>
+              <li>Execute SEO strategies</li>
+              <li>Performing bug fixes</li>
+            </ul>
+          </article>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 export default Services;
